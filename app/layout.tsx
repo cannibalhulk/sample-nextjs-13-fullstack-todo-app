5a94730743c4aa08c16c73b481566bab66a5d5a6
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2023-01-03 13:37:13 +0400

    changed root title for the page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-
+import Header from './Header'
 export default function RootLayout({
   children,
 }: {
@@ -7,12 +7,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+      <head>
+        <title>Demo Next.js Search and Todo App</title>
+      </head>
+      <body>
+        <Header/>
+        {children}
+      </body>
     </html>
   )
 }
